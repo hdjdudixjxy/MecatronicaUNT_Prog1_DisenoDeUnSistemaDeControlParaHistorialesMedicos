76f6fb76f674bb3cad67074ddf35219aa2515c82
hdjdudijxy <crisaaron_1@hotmail.com>
2022-07-26 16:55:14 -0500

    .

--- PROYECTO_UNT/PROGRAMA.py
+++ PROYECTO_UNT/PROGRAMA.py
@@ -2,13 +2,16 @@ import tkinter as tk
 import sqlite3 as sql
 
 def main():
-    base = tk.Tk()
-    base.title("HISTORIAS CLÍNICAS")
-    base.resizable()
-    
-    base.mainloop()
+    base = tk.Tk() 
+    base.iconbitmap("ICONO.ico") # Agregue un icono 
+    base.title("HISTORIAS CLÍNICAS") 
+    base.resizable(width=True, height=True) #Permite maximizar la ventana
+    base.minsize(width=1000,height=800) # Tamaño mínimo
+
+    ventana = tk.Frame(base)
+    ventana.mainloop()
 
 con=sql.connect("BaseDeDatos.db")
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     main()
