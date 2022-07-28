b9897a6f4be6b472cbcd4de54dcc8a25b2e876de
hdjdudijxy <crisaaron_1@hotmail.com>
2022-07-28 12:06:47 -0500

    .

---
+++ PROYECTO_UNT_HISTORIAL_MEDICO/PROGRAMA.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from INTERFAZ.GUI import Frame
+from INTERFAZ.GUI import error
+
+def main():
+    aplicacion = tk.Tk() # interfaz principal
+    aplicacion.title('HISTORIAS CLINICAS') # nombre de la interfaz
+    aplicacion.resizable(width=True, height=True) # expansión a pantalla completa
+    aplicacion.geometry("1280x720+100+50") # tamaño por defecto y posición
+    aplicacion.minsize(width=1280, height=720) # tamaño mínimo al minimizar
+    aplicacion.iconbitmap('ICONOS/ICONO.ico') # icono de la interfaz
+    fondo = Frame(aplicacion) # ventana para dar color de fondo
+    fondo.mainloop() # bucle generador
+try:
+    if __name__ == '__main__':
+        main()
+except:
+    error()
+
+#### VARIABLE-LEYENDA #######
+# lbl"..." : significa Label
+# sv "..." : es la variable de los entrys
+# entry "..." : significa entry
+# btn "..." : significa button
+
+
+
+
+
+
+
+
